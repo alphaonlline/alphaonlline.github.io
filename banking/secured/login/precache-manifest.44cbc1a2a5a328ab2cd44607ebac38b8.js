06a4ac08ceb1c3971cdf09fcabe689e6b0db9508
unknown <andrew.zavatin@yahoo.com>
2020-07-12 10:24:13 +0300

    xx

---
+++ banking/secured/login/precache-manifest.44cbc1a2a5a328ab2cd44607ebac38b8.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0801fce25648667e02163634d88cb678",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a3d39925b31688bd5a4f",
+    "url": "/static/css/main.497e7c26.chunk.css"
+  },
+  {
+    "revision": "a122caa4331a38d71ee9",
+    "url": "/static/js/2.fc628521.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fc628521.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3d39925b31688bd5a4f",
+    "url": "/static/js/main.c3a42300.chunk.js"
+  },
+  {
+    "revision": "921771ecadd3f378106d",
+    "url": "/static/js/runtime-main.3fff5854.js"
+  },
+  {
+    "revision": "5d7c31b284ddb01fc1cbae0edc9ec210",
+    "url": "/static/media/Calibri_Regular.5d7c31b2.ttf"
+  },
+  {
+    "revision": "6379ed4c8a50ae389bb49be90215e2cb",
+    "url": "/static/media/Phone1.6379ed4c.svg"
+  },
+  {
+    "revision": "79828fe584d22f23db81d2d9dc112453",
+    "url": "/static/media/Preloader.79828fe5.svg"
+  },
+  {
+    "revision": "2574c4f2356f00a774e98f073c263510",
+    "url": "/static/media/closed.2574c4f2.svg"
+  },
+  {
+    "revision": "fed6239c7d8fcbe256f253385857d654",
+    "url": "/static/media/facebook.fed6239c.svg"
+  },
+  {
+    "revision": "3e0210d2e9dd9806ccc404f08f5f910c",
+    "url": "/static/media/gb.3e0210d2.svg"
+  },
+  {
+    "revision": "7662fa5b3a541fbbd3c1c14543150d33",
+    "url": "/static/media/linkedin.7662fa5b.svg"
+  },
+  {
+    "revision": "4625c4072893ec78c429f0dc8f10cfd9",
+    "url": "/static/media/logo-blue.4625c407.svg"
+  },
+  {
+    "revision": "f5eba4c03e8ccb17bc7d3ee71e919fa7",
+    "url": "/static/media/open.f5eba4c0.svg"
+  },
+  {
+    "revision": "79b4d05a034fbf1a5094396b382f6290",
+    "url": "/static/media/twitter.79b4d05a.svg"
+  },
+  {
+    "revision": "eb6abdec54a5e8a1c1aeaf8320603028",
+    "url": "/static/media/user_login.eb6abdec.PNG"
+  },
+  {
+    "revision": "4a32d3aea3209ebc99f68ec9dbfeb7d3",
+    "url": "/static/media/user_pass.4a32d3ae.PNG"
+  },
+  {
+    "revision": "4bd715444376a5fb4339c8c2715d2fd3",
+    "url": "/static/media/youtube.4bd71544.svg"
+  }
+]);
